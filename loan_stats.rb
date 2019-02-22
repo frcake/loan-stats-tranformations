0f07f427ad7e5cfd3ef5f25c5e705e1a864d1391
frcake <mpdevcake@gmail.com>
2019-02-22 15:47:25 +0200

    Add comments

--- loan_stats.rb
+++ loan_stats.rb
@@ -1,17 +1,23 @@
 require 'csv'
 
-v1 = ARGV[0]
+# read args (csv_path)
+csv_path = ARGV[0]
 
-csv = CSV.read(v1) # .map { |row| row[1] }
+# read csv file in rows
+csv = CSV.read(csv_path)
+
+# init hash
 csv_hash = {}
 
+# create a hash key for each column name
 csv[0].each do |key|
   csv_hash[key] = nil
 end
-p csv_hash.size
 
+# read csv in columns and pass the value as an array
+# to each respective hash key
+# now we have the csv as a hash so refer to each column
+# using the hash
 csv_hash.each_with_index do |(key, _value), i|
   csv_hash[key] = csv.map { |row| row[i] }.reject.each_with_index { |_item, index| index == 0 }
 end
-
-p csv_hash
